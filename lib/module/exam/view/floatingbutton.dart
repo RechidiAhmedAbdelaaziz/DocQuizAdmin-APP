32c023dbb8e469e044005d4b4c03e8b76cdbf770
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-08-23 23:19:51 +0100

    **Add exam module UI components**
    Introduced new UI components for the exam module, including a list of exams, individual exam items with details, and a floating action button. These changes establish a more structured and visually appealing interface for users to interact with available exams, enhancing the overall user experience within the app. Components were designed with versatility in mind, ensuring adaptability for future features and improvements.
    
    - The `ExamList` component dynamically displays exams leveraging the `ExamCubit`'s state.
    - Individual exam items (`ExamItem`) present key details such as title, question count, and allocated time, improving clarity and accessibility.
    - A placeholder floating action button (`FloatingButton`) is introduced, laying groundwork for future functionalities like creating or sorting exams.
    - A scaffold (`Scaffold`) ties the components together, providing a cohesive structure that incorporates an app bar, the exam list body, and the floating action button.
    
    These additions mark a significant step towards a comprehensive exam management feature, facilitating easier navigation and interaction for end-users. Further adjustments and feature integrations can be built upon this foundational work.

---
+++ lib/module/exam/view/floatingbutton.dart
@@ -0,0 +1,10 @@
+part of 'exam.screen.dart';
+
+class _FloatingButton extends StatelessWidget {
+  const _FloatingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
