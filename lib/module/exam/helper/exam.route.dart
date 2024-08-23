de9adccb171606467c43bd82b4c526d5c5393a5e
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-08-23 23:19:07 +0100

    feat: Add ExamRoute for navigating to the exam screen

---
+++ lib/module/exam/helper/exam.route.dart
@@ -0,0 +1,8 @@
+import 'package:admin_app/core/router/abstract_route.dart';
+import 'package:admin_app/module/exam/view/exam.screen.dart';
+
+class ExamRoute extends AbstractRoute {
+  static const route = '/exam';
+
+  ExamRoute() : super(route, child: const ExamScreen());
+}
