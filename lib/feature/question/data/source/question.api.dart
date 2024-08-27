7dc619b8376b330eb6a73d39fb801819a57362c8
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-08-27 18:36:42 +0100

    Add Question API Service
    Introduced API service for question management including CRUD operations and fetching with dynamic queries. This service leverages Retrofit for network communication, supporting the creation, update, deletion, and retrieval of questions. This amalgamation facilitates more streamlined and efficient management of question data within the admin app, while also providing a foundation for future enhancements like filtering and pagination.
    
    - Implemented Retrofit annotations to define HTTP methods and paths
    - Utilized Dio for request handling, ensuring robust error logging and response parsing
    - Ensured compatibility with existing network layer by integrating with admin and user API constants
    
    This enhancement aligns with our goal to improve app maintainability and scalability, potentially impacting how admin users interact with question data.

---
+++ lib/feature/question/data/source/question.api.dart
@@ -0,0 +1,34 @@
+import 'package:admin_app/core/network/constants/adminapi.constants.dart';
+import 'package:admin_app/core/network/constants/userapi.constants.dart';
+import 'package:admin_app/core/network/models/api_response.model.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'question.api.g.dart';
+
+@RestApi()
+abstract class QuestionApiService {
+  factory QuestionApiService(Dio dio, {String baseUrl}) =
+      _QuestionApiService;
+
+  @POST(AdminApiConstants.question)
+  Future<DataResponse> createQuestion(
+      @Body() Map<String, dynamic> body);
+
+  @PATCH(AdminApiConstants.questionWithId)
+  Future<DataResponse> updateQuestion(
+    @Path('id') String id,
+    @Body() Map<String, dynamic> body,
+  );
+
+  @DELETE(AdminApiConstants.questionWithId)
+  Future<MessageResponse> deleteQuestion(
+    @Path('id') String id,
+  );
+
+  @GET(UserApiConstants.question)
+  Future<PaginatedDataResponse> getQuestions({
+    @Queries() required Map<String, dynamic> queries,
+    @Body() required Map<String, dynamic> body,
+  });
+}
